cc37d4cc4a3a2a30144161f7a2446ce773b222d3
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-11-05 23:00:15 +0330

    change sampler

--- Dante/Shaders/ShaderFiles/DefaultPS.hlsl
+++ Dante/Shaders/ShaderFiles/DefaultPS.hlsl
@@ -1,12 +1,12 @@
 
 Texture2D albedoTex : register(t0);
 
-SamplerState gsamPointWrap : register(s0);
-SamplerState gsamPointClamp : register(s1);
-SamplerState gsamLinearWrap : register(s2);
-SamplerState gsamLinearClamp : register(s3);
-SamplerState gsamAnisotropicWrap : register(s4);
-SamplerState gsamAnisotropicClamp : register(s5);
+SamplerState samPointWrap : register(s0);
+SamplerState samPointClamp : register(s1);
+SamplerState samLinearWrap : register(s2);
+SamplerState samLinearClamp : register(s3);
+SamplerState samAnisotropicWrap : register(s4);
+SamplerState samAnisotropicClamp : register(s5);
 
 struct PS_Input
 {
@@ -17,5 +17,5 @@ struct PS_Input
 
 float4 main(PS_Input input) : SV_TARGET
 {
-    return albedoTex.Sample(gsamLinearWrap, input.tc);
+    return albedoTex.Sample(samAnisotropicWrap, input.tc);
 }
